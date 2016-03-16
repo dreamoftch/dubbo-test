d54ee1bc83e3dcfc9417d83f69228f705f156581
tianchaohui <tianchaohui.sh@superjia.com>
2016-03-16 16:31:09 +0800

    dubbo test

---
+++ dubbo-api/src/main/java/com/tch/test/dubbo/service/MyService.java
@@ -0,0 +1,9 @@
+package com.tch.test.dubbo.service;
+
+public interface MyService {
+
+    String getCurrentTime();
+    
+    String helloWorld();
+
+}
--- dubbo-provider/src/main/java/com/tch/test/dubbo/provider/serviceimpl/MyServiceImpl.java
+++ dubbo-provider/src/main/java/com/tch/test/dubbo/provider/serviceimpl/MyServiceImpl.java
@@ -2,18 +2,23 @@ package com.tch.test.dubbo.provider.serviceimpl;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import com.tch.test.dubbo.service.MyService;
 
 public class MyServiceImpl implements MyService {
 
+    private static final AtomicInteger atomicInteger = new AtomicInteger(1);
+    
+    @Override
     public String getCurrentTime() {
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd MM:hh:ss");
         return "当前时间：" + format.format(new Date());
     }
 
+    @Override
     public String helloWorld() {
-        return "helloWorld";
+        return "helloWorld-" + atomicInteger.getAndIncrement();
     }
 
 }
